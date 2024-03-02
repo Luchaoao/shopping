ac938dc421be624cac6695df98f9e71caab355e1
Lucha <73822723+Luchaoao@users.noreply.github.com>
2024-03-02 11:02:47 +0800

    buy finish

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
-    <RouterView />
-  </div>
+  <header class="bg-[#1F363D] text-white">
+    <div class="wrapper p-2">
+      <nav>
+        <RouterLink to="/" class="ms-2">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,325 @@
 export default {
 	data() {
 		return {
-			
+
 		};
 	},
-	computed: {
-
-	},
 	methods: {
-		
+
 	},
 };
 </script>
 
 <template>
-
+	<main>
+		<div class="font-bold text-3xl mt-3 ms-3 text-white">商品列表</div>
+		<div class="flex justify-end me-3">
+			<img src="@/assets/images/網狀.png" alt="">
+			<img src="@/assets/images/條列.png" alt="">
+		</div>
+		<div class="flex gap-5 flex-wrap justify-center mt-5">
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+			<div class="w-1/6 border-[1px] rounded-lg flex flex-col mb-5">
+				<div>
+					<div>
+						<img src="@/assets/images/300x300.png" alt="">
+					</div>
+					<div class="bg-slate-100 h-[250px] bg-opacity-30 pl-3 flex flex-col gap-3 pt-3">
+						<div class="font-bold text-2xl">Bitcoin</div>
+						<div>Immunization not carried out because of caregiver refusal</div>
+						<div>價格: 954</div>
+						<div class="flex mb-2">數量：
+							<div class="ms-5 border mb-3 h-[35px] flex justify-center items-center rounded-l-3xl p-2">
+								<img src="@/assets/images/minus.png" alt="">
+							</div>
+							<div
+								class="border border-x-0 border-y-1 h-[35px] flex justify-center items-center text-xl px-1">
+								<input type="number" min="1" class="h-[30px] w-[100px] border-0 text-center"
+									placeholder="1">
+							</div>
+							<div class="border h-[35px] flex justify-center items-center rounded-r-3xl p-2">
+								<img src="@/assets/images/plus.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full flex justify-center items-center border-[#e5e7eb] bg-[#50468c] text-white rounded-b-lg px-4 py-1 cursor-pointer ">
+						<img src="@/assets/images/cart.png" alt="">
+						加入購物車
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
